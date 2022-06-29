2103e51304011a4675dd0ad6817e86bb448b80bf
fes-too <58209794+fes-too@users.noreply.github.com>
2022-06-29 12:40:24 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EventNoteIcon />
+        Ochieng's Notebook
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
